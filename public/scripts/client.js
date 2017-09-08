b213e28f5a423b757ccd379797e45bf73a3b69a6
Alicia Wilking <aliciakaywilking@gmail.com>
2017-09-08 13:09:22 -0500

    finished basic game logic

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,10 +1,12 @@
 $(document).ready(onReady);
 
 var maxNumber = 0;
+var cpuNum = 0;
 
 function onReady() {
     console.log('jQuery sourced!');
     $('#submitSetup').on('click', submitSetup);
+    $('#submitGuesses').on('click', roundCheck);
 };
 
 function submitSetup() {
@@ -21,11 +23,37 @@ function submitSetup() {
     } else {
         console.log("wtf!?");
         maxNumber = 0;
-    }
-    return maxNumber;
+    };
+    cpuNum = generator(maxNumber);
+    return cpuNum;
 }
 
 function generator(anyNumber) {
-    var randomNumber = Math.floor(Math.random() * anyNumber);
+    var randomNumber = Math.floor(Math.random() * anyNumber)+1;
     return randomNumber;
 }
+
+//var cpuNum = generator(maxNumber);
+
+function checkGuess(playerGuess, cpuGuess) {
+    if (playerGuess > cpuGuess) {
+        console.log('Too high!')
+    } else if (playerGuess < cpuGuess) {
+        console.log('Too low!')
+    } else if (playerGuess == cpuGuess) {
+        console.log('YOU WIN!!')
+    } else {
+        console.log('You broke the game >:C')
+    }
+}
+
+function roundCheck() {
+    var player1 = $('#guess1').val();
+    var player2 = $('#guess2').val();
+    var player3 = $('#guess3').val();
+    var player4 = $('#guess4').val();
+    checkGuess(player1, cpuNum);
+    checkGuess(player2, cpuNum);
+    checkGuess(player3, cpuNum);
+    checkGuess(player4, cpuNum);
+};
